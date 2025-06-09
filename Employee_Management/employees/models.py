4418e3850d39572898fbdfc34302f4cb1309b468
Wafula206 <wafulabrian206@gmail.com>
2025-06-09 16:00:57 +0300

    employee_ms

---
+++ Employee_Management/employees/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+class Employee(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    position = models.CharField(max_length=100)
+    hire_date = models.DateField()
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
